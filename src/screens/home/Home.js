55f34ba574c7692322912673212bf72d13e5cc91
Narendra <69212699+narendra-gowda@users.noreply.github.com>
2020-11-13 15:23:30 +0530

    Added genres and artist filters

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -3,16 +3,22 @@ import './Home.css';
 import Header from '../../common/header/Header'
 import {withStyles} from '@material-ui/core/styles';
 import moviesData from '../../common/moviesData';
+import genres from '../../common/genres';
+import artists from '../../common/artists';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import moment from 'moment';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import { Typography } from '@material-ui/core';
+import { Checkbox, MenuItem, Typography } from '@material-ui/core';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
+import Select from '@material-ui/core/Select';
+// import Checkbox from '@material-ui/core/Checkbox';
+import ListItemText from '@material-ui/core/ListItemText';
+// import MenuItem from '@material-ui/core/MenuItem';
 
 const styles = theme => ({
   root: {
@@ -55,11 +61,19 @@ class Home extends Component{
   constructor(){
     super();
     this.state= {
-      movieName: ''
+      movieName: '',
+      genres: [],
+      artists: []
     };
   }
   movieNameHandler = (e) =>{
     this.setState({movieName: e.target.value});
+  }
+  genreChangeHandler = (e) => {
+    this.setState({genres: e.target.value});
+  }
+  artistChangeHandler = (e) => {
+    this.setState({artists: e.target.value});
   }
   render(){
     const {classes} = this.props;
@@ -99,6 +113,40 @@ class Home extends Component{
               <FormControl className={classes.formControl}>
                 <InputLabel htmlFor="movieName">Movie Name</InputLabel>
                 <Input id="movieName" onChange={this.movieNameHandler}/>
+              </FormControl>
+              <FormControl className={classes.formControl}>
+                <InputLabel htmlFor="genre-multi-select">Genre</InputLabel>
+                <Select
+                multiple={true}
+                input={<Input id="genre-multi-select" />}
+                renderValue={selected => selected.join(',')}
+                value={this.state.genres}
+                onChange={this.genreChangeHandler}>
+                    <MenuItem value="0">None</MenuItem>
+                    {genres.map(genre => (
+                      <MenuItem key={genre.id} value={genre.name}>
+                        <Checkbox checked={this.state.genres.indexOf(genre.name) > -1} />
+                        <ListItemText primary={genre.name} />
+                      </MenuItem>
+                    ))}
+                </Select>
+              </FormControl>
+              <FormControl className={classes.formControl}>
+                <InputLabel htmlFor="artist-multi-select">ARTISTS</InputLabel>
+                <Select
+                  multiple
+                  input={<Input id="artist-multi-select" />}
+                  value={this.state.artists}
+                  renderValue={selected => selected.join(',')}
+                  onChange={this.artistChangeHandler}>
+                    <MenuItem value="0">None</MenuItem>
+                    {artists.map(artist => (
+                      <MenuItem key={artist.id} value={artist.first_name+" ".concat(artist.last_name)}>
+                        <Checkbox checked={this.state.artists.indexOf(artist.first_name+" ".concat(artist.last_name)) > -1} />
+                        <ListItemText primary={artist.first_name+" ".concat(artist.last_name)} />
+                      </MenuItem>
+                    ))}
+                  </Select>
               </FormControl>
             </CardContent>
           </Card>
