f42cc8827100147c594c8e8242c079fffa52d7f8
Narendra <69212699+narendra-gowda@users.noreply.github.com>
2020-11-13 11:34:32 +0530

    Added released movies grid and contents

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -6,6 +6,7 @@ import moviesData from '../../common/moviesData';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
+import moment from 'moment';
 
 const styles = theme => ({
   root: {
@@ -22,8 +23,17 @@ upcomingMoviesHeading: {
 gridListUpcomingMovies: {
     flexWrap: 'nowrap',
     transform: 'translateZ(0)',
+    width: '100%',    
+  },
+  imageTile:{
+    height: '100%',
     width: '100%'
-}
+  },
+  gridList: {
+    width: 780,
+    height: 450,
+    transform: 'translateZ(0)'
+   },
 });
 
 class Home extends Component{
@@ -38,13 +48,26 @@ class Home extends Component{
         <GridList cols={5} className={classes.gridListUpcomingMovies}>
           {
             moviesData.map(movie => (
-              <GridListTile key={movie.id}>
-                <img src={movie.poster_url} alt={movie.title} />
+              <GridListTile key={movie.id} style={{height:270}}>
+                <img src={movie.poster_url} alt={movie.title} className={classes.imageTile}/>
             <GridListTileBar title={movie.title}></GridListTileBar>
               </GridListTile>
             ))
           }
         </GridList>
+        <div className="flex-container">
+        <div className="released-movies">
+        <GridList cols={3} spacing={25} className={classes.gridList}>
+          {moviesData.map(movie =>(
+            <GridListTile key={movie.id} style={{height:370, cursor: 'pointer'}}>
+              <img src={movie.poster_url} alt={movie.title} className={classes.imageTile}/>
+          <GridListTileBar title={movie.title} subtitle={<span>Release Date: {moment(movie.release_date).format("ddd MMM DD YYYY")}</span>}></GridListTileBar>
+            </GridListTile>
+          ))} 
+        </GridList>
+        </div>
+        <div className="filters"></div>
+        </div>
       </div>
     )
   }
