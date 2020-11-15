799c86c467e36da2ddce70ecb8ac516be4d6d433
Narendra <69212699+narendra-gowda@users.noreply.github.com>
2020-11-15 22:46:59 +0530

    Created a Card to book tickets

--- src/screens/bookTicket/BookTicket.js
+++ src/screens/bookTicket/BookTicket.js
@@ -6,10 +6,60 @@ import Home from '../../screens/home/Home';
 import './BookTicket.css';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import location from '../../common/location';
+import language from '../../common/language';
+import showDate from '../../common/showDate';
+import showTime from '../../common/showTime';
+import MenuItem from '@material-ui/core/MenuItem';
+import Input from '@material-ui/core/Input';
+import Button from '@material-ui/core/Button';
+import FormHelperText from '@material-ui/core/FormHelperText';
 
 class BookTicket extends Component{
+  constructor(){
+    super();
+    this.state= {
+      location: '',
+      language: '',
+      showDate: '',
+      showTime: '',
+      tickets: 0,
+      availableTickets: 20,
+      unitPrice: 500,
+      locationRequired: 'dispNone',
+      langRequired: 'dispNone',
+      dateRequired: 'dispNone',
+      timeRequired: 'dispNone',
+      ticketRequired: 'dispNone',
+    }
+  }
   backToHomeHandler = () =>{
     ReactDOM.render(<Home />, document.getElementById('root'));
+  }
+  locationHandler = (e) => {
+    this.setState({location : e.target.value});
+  }
+  languageHandler = (e) =>{
+    this.setState({language : e.target.value});
+  }
+  showDateHandler = (e) =>{
+    this.setState({showDate : e.target.value});
+  }
+  showTimeHandler = (e) =>{
+    this.setState({showTime : e.target.value});
+  }
+  ticketChangeHandler = (e) =>{
+    this.setState({tickets : e.target.value});
+  }
+  bookTicketsHandler = () =>{
+    this.state.location === '' ? this.setState({locationRequired: 'dispBlock'}) : this.setState({locationRequired: 'dispNone'});
+    this.state.language === '' ? this.setState({langRequired: 'dispBlock'}) : this.setState({langRequired: 'dispNone'});
+    this.state.showDate === '' ? this.setState({dateRequired: 'dispBlock'}) : this.setState({dateRequired: 'dispNone'});
+    this.state.showTime === '' ? this.setState({timeRequired: 'dispBlock'}) : this.setState({timeRequired: 'dispNone'});
+    this.state.tickets <= 0 ? this.setState({ticketRequired: 'dispBlock'}) : this.setState({ticketRequired: 'dispNone'});
   }
   render(){
     return(
@@ -18,14 +68,65 @@ class BookTicket extends Component{
         <div className="back-btn">
         <Typography onClick={this.backToHomeHandler}>&#60; Back to Movie Details</Typography>
         </div>
-        <div className="booking-card">
-          <Card>
+        
+          <Card className="cardStyle">
             <CardContent>
-              <Typography variant="h6">BOOK TICKET</Typography>
+              <Typography variant="h6" color="primary">BOOK TICKET</Typography><br />
+                <FormControl className="form-control" required>
+                  <InputLabel htmlFor="location">Choose Location</InputLabel>
+                  <Select 
+                    value={this.state.location}
+                    onChange={this.locationHandler}>
+                      {location.map(loc => (
+                        <MenuItem key={'loc'+loc.id} value={loc.location}>{loc.location}</MenuItem>
+                      ))}
+                    </Select>
+                    <FormHelperText className={this.state.locationRequired} style={{color:'red'}}>required</FormHelperText>
+                </FormControl><br />
+                <FormControl className="form-control" required>
+                  <InputLabel htmlFor="language">Choose Language</InputLabel>
+                  <Select 
+                    value={this.state.language}
+                    onChange={this.languageHandler}>
+                      {language.map(lan => (
+                        <MenuItem key={'lan'+lan.id} value={lan.language}>{lan.language}</MenuItem>
+                      ))}
+                    </Select>
+                    <FormHelperText className={this.state.langRequired} style={{color:'red'}}>required</FormHelperText>
+                </FormControl><br/>
+                <FormControl className="form-control" required>
+                  <InputLabel htmlFor="location">Choose Show Date:</InputLabel>
+                  <Select 
+                    value={this.state.showDate}
+                    onChange={this.showDateHandler}>
+                      {showDate.map(date => (
+                        <MenuItem key={'date'+date.id} value={date.showDate}>{date.showDate}</MenuItem>
+                      ))}
+                    </Select>
+                    <FormHelperText className={this.state.dateRequired} style={{color:'red'}}>required</FormHelperText>
+                </FormControl><br />
+                <FormControl className="form-control" required>
+                  <InputLabel htmlFor="location">Choose Show Time:</InputLabel>
+                  <Select 
+                    value={this.state.showTime}
+                    onChange={this.showTimeHandler}>
+                      {showTime.map(time => (
+                        <MenuItem key={'time'+time.id} value={time.showTime}>{time.showTime}</MenuItem>
+                      ))}
+                    </Select>
+                    <FormHelperText className={this.state.timeRequired} style={{color:'red'}}>required</FormHelperText>
+                </FormControl><br />   
+                <FormControl className="form-control" required>
+                  <InputLabel htmlFor="tickets">Tickets: ( {this.state.availableTickets} Tickets available )</InputLabel>
+                  <Input id="tickets" value={this.state.tickets !== 0 ? this.state.tickets : "" } onChange={this.ticketChangeHandler} />
+                  <FormHelperText className={this.state.ticketRequired} style={{color:'red'}}>required</FormHelperText>
+                </FormControl><br />
+                <Typography className="left">Unit Price: Rs. {this.state.unitPrice}</Typography><br />
+                <Typography className="left">Total Price: Rs. {this.state.unitPrice * this.state.tickets}</Typography><br />
+                <Button  variant="contained" color="primary" onClick={this.bookTicketsHandler}>BOOK</Button>    
             </CardContent>
           </Card>
         </div>
-      </div>
     );
   }
 }
